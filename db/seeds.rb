44e33578a48cf42961feda3c869149e23c7c356f
Jeff-Adler <Jeffm.adler@gmail.com>
2020-12-31 19:28:38 -0500

    increased alt count in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,12 @@ Alt.destroy_all
 
 20.times do 
     author = Author.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name )
-    book = Book.create(title: Faker::Book.title)
-    AuthorBook.create(author_id: author.id, book_id: book.id)
+    book1 = Book.create(title: Faker::Book.title)
+    book2 = Book.create(title: Faker::Book.title)
+    AuthorBook.create(author_id: author.id, book_id: book1.id)
+    AuthorBook.create(author_id: author.id, book_id: book2.id)
 end
 
-40.times do 
+80.times do 
     Book.all.sample.alts.create(title: Faker::Book.title)
 end
