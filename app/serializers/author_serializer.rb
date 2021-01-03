0f7600f912f97d19eee8f4970fd56ccac692096e
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-03 10:05:39 -0500

    added sorting to nested lists

--- app/serializers/author_serializer.rb
+++ app/serializers/author_serializer.rb
@@ -1,4 +1,8 @@
 class AuthorSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name
   has_many :books, through: :author_books
+
+  def books
+    object.books.order("title")
+  end
 end
--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -2,4 +2,12 @@ class BookSerializer < ActiveModel::Serializer
   attributes :id, :title
   has_many :authors, through: :author_books
   has_many :alts
+
+  def authors
+    object.authors.order("last_name")
+  end
+
+  def alts
+    object.alts.order("title")
+  end
 end
